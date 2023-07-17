ecf9a5f897d10f00159e099e4cf93daeff3de469
OA2302 <okikiadenekan3@gmail.com>
2023-07-17 23:27:30 +0100

    first commit

--- src/components/Home/Index.js
+++ src/components/Home/Index.js
@@ -1,5 +1,5 @@
 import {useEffect, useState} from 'react';
-import Logo from '../../assets/images/me.jpg';
+import Logo from '../../assets/images/LOGO.jpeg';
 import { Link } from 'react-router-dom';
 import './Home.scss';
 import AnimatedLetter from '../AnimatedLetters/Index';
